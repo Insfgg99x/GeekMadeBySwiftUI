a39b3c74a874c5be5be3852c6090c17e8480c165
xgf <xiaguifeng@yunzujia.com>
2020-01-13 18:03:27 +0800

    none

---
+++ Category/ImageBrowser.swift
@@ -0,0 +1,21 @@
+//
+//  ImageBrowser.swift
+//  GeekMadeBySwiftUI
+//
+//  Created by xgf on 2020/1/13.
+//  Copyright © 2020 xgf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImageBrowser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageBrowser_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageBrowser()
+    }
+}
